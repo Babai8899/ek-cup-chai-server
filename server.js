00b06e1bf17e5582be44c69c8bc75f53b4b53806
Babai8899 <indra.babai.9898@gmail.com>
2026-01-05 08:27:54 +0530

    Port configuration done

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ import express from "express";
 import locationRoutes from "./routes/location.js";
 import distanceRoutes from "./routes/distance.js";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
@@ -21,7 +26,7 @@ app.get("/", (req, res) => {
 app.use("/api/location", locationRoutes);
 app.use("/api/distance", distanceRoutes);
 
-app.listen(5000, "0.0.0.0", () => {
-  console.log("Server running on port 5000");
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
